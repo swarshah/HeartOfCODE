747ea8331dc795272db3c2a42e34dd2fb69d8158
Bino Oommen Samuel <binooommen37@gmail.com>
2015-02-22 00:20:05 -0500

    without div toggle

--- business_solutions.php
+++ business_solutions.php
@@ -46,129 +46,113 @@ function close_db(){
 connect();
 // print_r($_POST);
 ?>
+<script>
+$(document).ready(function(){
+    $(".butn").click(function(this){
+        $("this").fadeToggle("slow");
+    });
+});
+</script>
 	<body class="homepage">
-		<h2 style="text-align: center;"><a href="#">Listed out according to the major cities.</a></h2>
+		<h2 style="text-align: center;margin-top: 15px;margin-bottom: 50px;"><a href="#">Listed out according to the major cities.</a></h2>
 		<!-- Banner -->
-			<div style="float:left;background-color:#2b252c;">
-			<section id="banner" style="background-color:#2b252c;">
-			
-					<article id="main" class="container special" style="width: 400px;float: left;margin-left: 28px;margin-right: 15px;">
-						<form id="business-form" method="POST">
-							<label for="province">Province</label>
-							<select id="province" name="province">
-								<option id="default" name="default" value="0">All</option>
-								<?php 
-									$query = "SELECT p_id, code, name FROM province";
-									$result = mysql_query($query) or die(mysql_error()."[".$query."]");
-									$code="";
-									if(isset($_POST) and isset($_POST['province'])){ 
-										$code=$_POST['province'];
-									} 
-									while ($row = mysql_fetch_array($result))
-									{
-										$sel="";
-										if($code==$row['code']){ 
-											$sel="selected";
+			<div>
+				<div id="banner" style="float:left;background-color:#2b252c;">
+						<article id="main" class="container special" style="width: 300px;float: left;margin-left: 30px;margin-right: 30px;">
+							<form id="business-form" method="POST">
+								<label for="province">Province</label>
+								<select id="province" name="province">
+									<option id="default" name="default" value="0">All</option>
+									<?php 
+										$query = "SELECT p_id, code, name FROM province";
+										$result = mysql_query($query) or die(mysql_error()."[".$query."]");
+										$code="";
+										if(isset($_POST) and isset($_POST['province'])){ 
+											$code=$_POST['province'];
+										} 
+										while ($row = mysql_fetch_array($result))
+										{
+											$sel="";
+											if($code==$row['code']){ 
+												$sel="selected";
+											}
+											echo "<option value='".$row['code']."' ".$sel." >".$row['name']."</option>";
 										}
-										echo "<option value='".$row['code']."' ".$sel." >".$row['name']."</option>";
-									}
-								?> 
-							</select>
-							<label for="sector">Sector</label>
-							<select id="sector" name="sector">
-								<option id="default" name="default" value="0">--select--</option>
-								<?php 
-									$query = "SELECT id, name FROM sector";
-									$result = mysql_query($query) or die(mysql_error()."[".$query."]");
-									$code="";
-									if(isset($_POST) and isset($_POST['sector'])){ 
-										$code=$_POST['sector'];
-									} 
-									while ($row = mysql_fetch_array($result))
-									{
-										$sel="";
-										if($code==$row['id']){ 
-											$sel="selected";
+									?> 
+								</select>
+								<label for="sector">Sector</label>
+								<select id="sector" name="sector">
+									<option id="default" name="default" value="0">--select--</option>
+									<?php 
+										$query = "SELECT id, name FROM sector";
+										$result = mysql_query($query) or die(mysql_error()."[".$query."]");
+										$code="";
+										if(isset($_POST) and isset($_POST['sector'])){ 
+											$code=$_POST['sector'];
+										} 
+										while ($row = mysql_fetch_array($result))
+										{
+											$sel="";
+											if($code==$row['id']){ 
+												$sel="selected";
+											}
+											echo "<option value='".$row['id']."' ".$sel." >".$row['name']."</option>";
 										}
-										echo "<option value='".$row['id']."' ".$sel." >".$row['name']."</option>";
-									}
-								?> 
-							</select>
-							
-							<?php 
-								$value="";
-								if(isset($_POST) and isset($_POST['capital'])){ 
-									$value=$_POST['capital'];
-								} 
-							?>
-							<label for="capital">Initial Capital</label>
-							<select id="capital" name="capital">
-								<option id="default" name="default" value="0" >--select--</option>
-								<option value="1" <?php if($value==1){echo "selected";} ?> >1000 < 10000</option>
-								<option value="2" <?php if($value==2){echo "selected";} ?>>10000 - 50000</option>
-								<option value="3" <?php if($value==3){echo "selected";} ?>>> 50000</option> 
-							</select>
-							<label for="labour">No of Labour
-								<input type="text" id="labour" name="labour" value="<?php if($_POST and $_POST['labour']) { echo $_POST['labour'];} ?>">
-							</label>
-							<div>
-								<label for="subsidy">Subsidy</label>
-								<input type="checkbox" id="yes" name="subsidy">
-								<label for="yes">Yes</label>
-								<input type="checkbox" id="no" name="subsidy">
-								<label for="no">No</label>
-							</div>
-							
-							<footer>
-								<input type="submit" class="button" value="Go">
-								<!-- <a href="#" class="button">Go</a> -->
-							</footer>
-							
-						</form>
+									?> 
+								</select>
+								
+								<?php 
+									$value="";
+									if(isset($_POST) and isset($_POST['capital'])){ 
+										$value=$_POST['capital'];
+									} 
+								?>
+								<label for="capital">Initial Capital</label>
+								<select id="capital" name="capital">
+									<option id="default" name="default" value="0" >--select--</option>
+									<option value="1" <?php if($value==1){echo "selected";} ?> >1000 < 10000</option>
+									<option value="2" <?php if($value==2){echo "selected";} ?>>10000 - 50000</option>
+									<option value="3" <?php if($value==3){echo "selected";} ?>>> 50000</option> 
+								</select>
+								<label for="labour">No of Labour
+									<input type="text" id="labour" name="labour" value="<?php if(isset($_POST) and isset($_POST['labour']) and $_POST['labour']) { echo $_POST['labour'];} ?>">
+								</label>
+								
+								<div style="text-align: initial;">
+									<label style="display: inline;" for="subsidy">Subsidy</label>
+									<span style="margin-left: 40px;">
+										<input type="radio" id="yes" value="yes" name="subsidy"  <?php if(isset($_POST) and isset($_POST['subsidy']) and $_POST['subsidy']=='yes') { echo "checked";} ?>>
+										<label style="display: inline;" for="yes">Yes</label>
+									</span>
+									<span style="margin-left: 40px;">
+										<input type="radio" id="no" value="no" name="subsidy" <?php if(isset($_POST) and isset($_POST['subsidy']) and $_POST['subsidy']=='no') { echo "checked";} ?>>
+										<label style="display: inline;" for="no">No</label>
+									</span>
+								</div>
+								
+								
+								<footer>
+									<input type="submit" class="button" value="Go">
+								</footer>
+								
+							</form>
 
-					</article>
-			</section>
-		</div>
+						</article>
 
-		<!-- Features -->
-			<div class="wrapper style1" style="float: right;width: 1100px;">
-				
-				<section id="features" class=" special">
+				</div>
 
-					<div class="row">
-						<article class="4u special">
-							<a href="#" class="image featured"><img src="images/pic07.jpg" alt="" /></a>
-							<header>
-								<h3><a href="#">Gravida aliquam penatibus</a></h3>
-							</header>
+				<!-- Features -->
+				<div id="features" class="" style="margin-left: 360px;">
+					<?php foreach([1,2,3] as $i){?>
+					<div class="row" id="clickme<?php echo $i; ?>" style="padding: 0px 0 65px 100px;">
+							<h3><a href="#">Gravida aliquam penatibus</a><button class="button butn" value="Open">Open</button></h3>
 							<p>
 								Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
 								porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
 							</p>
-						</article>
-						<article class="4u special">
-							<a href="#" class="image featured"><img src="images/pic08.jpg" alt="" /></a>
-							<header>
-								<h3><a href="#">Sed quis rhoncus placerat</a></h3>
-							</header>
-							<p>
-								Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
-								porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
-							</p>
-						</article>
-						<article class="4u special">
-							<a href="#" class="image featured"><img src="images/pic09.jpg" alt="" /></a>
-							<header>
-								<h3><a href="#">Magna laoreet et aliquam</a></h3>
-							</header>
-							<p>
-								Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
-								porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
-							</p>
-						</article>
 					</div>
-				</section>
-
+					<?php }?>
+				</div>
 			</div>
 		<!-- Footer -->
 			<div id="footer" style="clear: both;">
--- index.php
+++ index.php
@@ -30,14 +30,25 @@
 	
 <?php
 
+
 function connect(){
 	$link = mysql_connect('localhost', 'root', '');
 	if (!$link) {
 		die('Could not connect: ' . mysql_error());
 	}
-	echo 'Connected successfully';
+	// echo 'Connected successfully';
+	
+	$db_selected = mysql_select_db('code', $link);
+	if (!$db_selected) {
+		die ('Can\'t use foo : ' . mysql_error());
+	}
+	return $link;
+}
+
+function close_db(){
 	mysql_close($link);
 }
+connect();
 
 ?>
 	<body class="homepage">
@@ -89,322 +100,76 @@ function connect(){
 			
 		<!-- Banner -->
 			<section id="banner">
-
-
-			
-				<div class="wrapper style2">
-
 					<article id="main" class="container special">
-						<!--<a href="#" class="image featured"><img src="images/pic06.jpg" alt="" /></a>-->
 						<header>
-							<h2><a href="#">Just a few questions for the right query</a></h2>
-							<p>
-								Sociis aenean eu aenean mollis mollis facilisis primis ornare penatibus aenean. Cursus ac enim 
-								pulvinar curabitur morbi convallis. Lectus malesuada sed fermentum dolore amet.
-							</p>
+							<h2><a href="#banner">Just a few questions for the right query</a></h2>
 						</header>
-						<p id="questions">
-							<label for="name">Name</label>
-							<input type="text" id="name" name="name">
+						<form id="questions-form" method="POST" action="business_solutions.php">
+							<label for="province">Province</label>
+							<select id="province" name="province">
+								<option id="default" name="default" value="0">All</option>
+								<?php 
+									$query = "SELECT p_id, code, name FROM province";
+									$result = mysql_query($query) or die(mysql_error()."[".$query."]");
+									while ($row = mysql_fetch_array($result))
+									{
+										echo "<option value='".$row['code']."'>".$row['name']."</option>";
+									}
+								?> 
+							</select>
 							<br>
 							<label for="sector">Sector</label>
 							<select id="sector" name="sector">
-								<option id="default" name="default">--select--</option>
-								<?php
-									
-								?>
+								<option id="default" name="default" value="0">--select--</option>
+								<?php 
+									$query = "SELECT id, name FROM sector";
+									$result = mysql_query($query) or die(mysql_error()."[".$query."]");
+									while ($row = mysql_fetch_array($result))
+									{
+										echo "<option value='".$row['id']."'>".$row['name']."</option>";
+									}
+								?> 
 							</select>
-						</p>
-						<p>
-							Commodo id natoque malesuada sollicitudin elit suscipit. Curae suspendisse mauris posuere accumsan massa 
-							posuere lacus convallis tellus interdum. Amet nullam fringilla nibh nulla convallis ut venenatis purus 
-							sit arcu sociis. Nunc fermentum adipiscing tempor cursus nascetur adipiscing adipiscing. Primis aliquam 
-							mus lacinia lobortis phasellus suscipit. Fermentum lobortis non tristique ante proin sociis accumsan 
-							lobortis. Auctor etiam porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum 
-							consequat integer interdum integer purus sapien. Nibh eleifend nulla nascetur pharetra commodo mi augue 
-							interdum tellus. Ornare cursus augue feugiat sodales velit lorem. Semper elementum ullamcorper lacinia 
-							natoque aenean scelerisque.
-						</p>
-						<footer>
-							<a href="#" class="button">Go</a>
-						</footer>
-					</article>
+							<br>
+							<label for="capital">Initial Capital</label>
+							<select id="capital" name="capital">
+								<option id="default" name="default" value="0">--select--</option>
+								<option value="1">1000 < 10000</option>
+								<option value="2">10000 - 50000</option>
+								<option value="3">> 50000</option> 
+							</select>
+							<br>
+							<label for="labour">No of Labour
+								<input type="text" id="labour" name="labour">
+							</label>
+							<br>
+							
+							<div style="text-align: initial;">
+								<label style="display: inline;" for="subsidy">Subsidy</label>
+								<span style="margin-left: 100px;">
+									<input type="radio" id="yes" value="yes" name="subsidy">
+									<label style="display: inline;" for="yes">Yes</label>
+								</span>
+								<span style="margin-left: 100px;">
+									<input type="radio" id="no" value="no" name="subsidy">
+									<label style="display: inline;" for="no">No</label>
+								</span>
+							</div>
+							
+							<footer>
+								<input type="submit" class="button" value="Go">
+								<!-- <a href="#" class="button">Go</a> -->
+							</footer>
+							
+						</form>
 
-				</div>
+					</article>
 			</section>
-			
-		<!-- Carousel -->
-			<section class="carousel">
-				<div class="reel">
-
-					<article>
-						<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
-						<header>
-							<h3><a href="#">Pulvinar sagittis congue</a></h3>
-						</header>
-						<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>							
-					</article>
-				
-					<article>
-						<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
-						<header>
-							<h3><a href="#">Fermentum sagittis proin</a></h3>
-						</header>
-						<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>							
-					</article>
-				
-					<article>
-						<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
-						<header>
-							<h3><a href="#">Sed quis rhoncus placerat</a></h3>
-						</header>
-						<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>							
-					</article>
-				
-					<article>
-						<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
-						<header>
-							<h3><a href="#">Ultrices urna sit lobortis</a></h3>
-						</header>
-						<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>							
-					</article>
-				
-					<article>
-						<a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
-						<header>
-							<h3><a href="#">Varius magnis sollicitudin</a></h3>
-						</header>
-						<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>							
-					</article>
-
-					<article>
-						<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
-						<header>
-							<h3><a href="#">Pulvinar sagittis congue</a></h3>
-						</header>
-						<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>							
-					</article>
-				
-					<article>
-						<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
-						<header>
-							<h3><a href="#">Fermentum sagittis proin</a></h3>
-						</header>
-						<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>							
-					</article>
-				
-					<article>
-						<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
-						<header>
-							<h3><a href="#">Sed quis rhoncus placerat</a></h3>
-						</header>
-						<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>							
-					</article>
-				
-					<article>
-						<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
-						<header>
-							<h3><a href="#">Ultrices urna sit lobortis</a></h3>
-						</header>
-						<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>							
-					</article>
-				
-					<article>
-						<a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
-						<header>
-							<h3><a href="#">Varius magnis sollicitudin</a></h3>
-						</header>
-						<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>							
-					</article>
-
-				</div>
-			</section>
-			
-		<!-- Main -->
-			<div class="wrapper style2">
-
-				<article id="main" class="container special">
-					<a href="#" class="image featured"><img src="images/pic06.jpg" alt="" /></a>
-					<header>
-						<h2><a href="#">Sed massa imperdiet magnis</a></h2>
-						<p>
-							Sociis aenean eu aenean mollis mollis facilisis primis ornare penatibus aenean. Cursus ac enim 
-							pulvinar curabitur morbi convallis. Lectus malesuada sed fermentum dolore amet.
-						</p>
-					</header>
-					<p>
-						Commodo id natoque malesuada sollicitudin elit suscipit. Curae suspendisse mauris posuere accumsan massa 
-						posuere lacus convallis tellus interdum. Amet nullam fringilla nibh nulla convallis ut venenatis purus 
-						sit arcu sociis. Nunc fermentum adipiscing tempor cursus nascetur adipiscing adipiscing. Primis aliquam 
-						mus lacinia lobortis phasellus suscipit. Fermentum lobortis non tristique ante proin sociis accumsan 
-						lobortis. Auctor etiam porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum 
-						consequat integer interdum integer purus sapien. Nibh eleifend nulla nascetur pharetra commodo mi augue 
-						interdum tellus. Ornare cursus augue feugiat sodales velit lorem. Semper elementum ullamcorper lacinia 
-						natoque aenean scelerisque.
-					</p>
-					<footer>
-						<a href="#" class="button">Continue Reading</a>
-					</footer>
-				</article>
-
-			</div>
-
-		<!-- Features -->
-			<div class="wrapper style1">
-				
-				<section id="features" class="container special">
-					<header>
-						<h2>Morbi ullamcorper et varius leo lacus</h2>
-						<p>Ipsum volutpat consectetur orci metus consequat imperdiet duis integer semper magna.</p>
-					</header>
-					<div class="row">
-						<article class="4u special">
-							<a href="#" class="image featured"><img src="images/pic07.jpg" alt="" /></a>
-							<header>
-								<h3><a href="#">Gravida aliquam penatibus</a></h3>
-							</header>
-							<p>
-								Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
-								porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
-							</p>
-						</article>
-						<article class="4u special">
-							<a href="#" class="image featured"><img src="images/pic08.jpg" alt="" /></a>
-							<header>
-								<h3><a href="#">Sed quis rhoncus placerat</a></h3>
-							</header>
-							<p>
-								Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
-								porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
-							</p>
-						</article>
-						<article class="4u special">
-							<a href="#" class="image featured"><img src="images/pic09.jpg" alt="" /></a>
-							<header>
-								<h3><a href="#">Magna laoreet et aliquam</a></h3>
-							</header>
-							<p>
-								Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
-								porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
-							</p>
-						</article>
-					</div>
-				</section>
-
-			</div>
+		
 
 		<!-- Footer -->
 			<div id="footer">
 				<div class="container">
-					<div class="row">
-						
-						<!-- Tweets -->
-							<section class="4u">
-								<header>
-									<h2 class="icon fa-twitter circled"><span class="label">Tweets</span></h2>
-								</header>
-								<ul class="divided">
-									<li>
-										<article class="tweet">
-											Amet nullam fringilla nibh nulla convallis tique ante sociis accumsan.
-											<span class="timestamp">5 minutes ago</span>
-										</article>
-									</li>
-									<li>
-										<article class="tweet">
-											Hendrerit rutrum quisque.
-											<span class="timestamp">30 minutes ago</span>
-										</article>
-									</li>
-									<li>
-										<article class="tweet">
-											Curabitur donec nulla massa laoreet nibh. Lorem praesent montes.
-											<span class="timestamp">3 hours ago</span>
-										</article>
-									</li>
-									<li>
-										<article class="tweet">
-											Lacus natoque cras rhoncus curae dignissim ultricies. Convallis orci aliquet.
-											<span class="timestamp">5 hours ago</span>
-										</article>
-									</li>
-								</ul>
-							</section>
-
-						<!-- Posts -->
-							<section class="4u">
-								<header>
-									<h2 class="icon fa-file circled"><span class="label">Posts</span></h2>
-								</header>
-								<ul class="divided">
-									<li>
-										<article class="post stub">
-											<header>
-												<h3><a href="#">Nisl fermentum integer</a></h3>
-											</header>
-											<span class="timestamp">3 hours ago</span>
-										</article>
-									</li>
-									<li>
-										<article class="post stub">
-											<header>
-												<h3><a href="#">Phasellus portitor lorem</a></h3>
-											</header>
-											<span class="timestamp">6 hours ago</span>
-										</article>
-									</li>
-									<li>
-										<article class="post stub">
-											<header>
-												<h3><a href="#">Magna tempus consequat</a></h3>
-											</header>
-											<span class="timestamp">Yesterday</span>
-										</article>
-									</li>
-									<li>
-										<article class="post stub">
-											<header>
-												<h3><a href="#">Feugiat lorem ipsum</a></h3>
-											</header>
-											<span class="timestamp">2 days ago</span>
-										</article>
-									</li>
-								</ul>
-							</section>
-
-						<!-- Photos -->
-							<section class="4u">
-								<header>
-									<h2 class="icon fa-camera circled"><span class="label">Photos</span></h2>
-								</header>
-								<div class="row 25% no-collapse">
-									<div class="6u">
-										<a href="#" class="image fit"><img src="images/pic10.jpg" alt="" /></a>
-									</div>
-									<div class="6u">
-										<a href="#" class="image fit"><img src="images/pic11.jpg" alt="" /></a>
-									</div>
-								</div>
-								<div class="row 25% no-collapse">
-									<div class="6u">
-										<a href="#" class="image fit"><img src="images/pic12.jpg" alt="" /></a>
-									</div>
-									<div class="6u">
-										<a href="#" class="image fit"><img src="images/pic13.jpg" alt="" /></a>
-									</div>
-								</div>
-								<div class="row 25% no-collapse">
-									<div class="6u">
-										<a href="#" class="image fit"><img src="images/pic14.jpg" alt="" /></a>
-									</div>
-									<div class="6u">
-										<a href="#" class="image fit"><img src="images/pic15.jpg" alt="" /></a>
-									</div>
-								</div>
-							</section>
-
-					</div>
-					<hr />
 					<div class="row">
 						<div class="12u">
 							
